ee86d5bbdf4e6359b3a2a355c4bec8d38b1e12ba
徐若芸 <jauneyh@xuruoyundeMacBook-Pro.local>
2019-03-13 21:06:26 +0800

    Update array in user's friend array

--- FirebasePractice/ViewController.swift
+++ FirebasePractice/ViewController.swift
@@ -12,7 +12,8 @@ import Firebase
 class ViewController: UIViewController {
     lazy var db = Firestore.firestore()
     var ref: DocumentReference? = nil
-    var userID: String?
+    var articleRef: DocumentReference? = nil
+    var userID: String = "YrVzf2PvU2ukDsTvQOxS"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,24 +24,24 @@ class ViewController: UIViewController {
         let time = NSDate(timeIntervalSince1970: TimeInterval(myTimeInterval))
         
         // Add a user with a generated ID
-        ref = db.collection("users").addDocument(data: [
-            "user_email": "jo3@mail.com",
-            "user_name": "jo3",
-            "friends": [
-                [
-                "id": "token1",
-                "statusCode": 1
-                ]
-            ]
-        ]) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(self.ref!.documentID)")
-            }
-        }
-        
-        self.userID = self.ref!.documentID
+//        ref = db.collection("users").addDocument(data: [
+//            "user_email": "jo3@mail.com",
+//            "user_name": "jo3",
+//            "friends": [
+//                [
+//                "id": "token1",
+//                "statusCode": 1
+//                ]
+//            ]
+//        ]) { err in
+//            if let err = err {
+//                print("Error adding document: \(err)")
+//            } else {
+//                print("Document added with ID: \(self.ref!.documentID)")
+//            }
+//        }
+//
+//        self.userID = self.ref!.documentID
         
         // Update user info with ID
 //        let usersRef = db.collection("users")
@@ -56,23 +57,23 @@ class ViewController: UIViewController {
 //            }
         
         
-        // add a new document in subcollection article
-        ref = db.collection("article").addDocument(data:
-            [
-                "article_content": "today is the best day",
-                "article_id": "byJo",
-                "article_tag": "SchoolLife",
-                "article_title": "Today",
-                "author": "\(userID)",
-                "created_time": time
-            ]
-        ) { err in
-                if let err = err {
-                    print("Error adding document: \(err)")
-                } else {
-                    print("Successfully add article to user, ID: \(self.ref!.documentID)")
-                }
-            }
+        // post article: add a new document in subcollection article
+//        articleRef = db.collection("article").addDocument(data:
+//            [
+//                "article_content": "today is the best day",
+//                "article_id": "byJo",
+//                "article_tag": "SchoolLife",
+//                "article_title": "Today",
+//                "author": "\(userID)",
+//                "created_time": time
+//            ]
+//        ) { err in
+//                if let err = err {
+//                    print("Error adding document: \(err)")
+//                } else {
+//                    print("Successfully add article, ID: \(self.articleRef!.documentID)")
+//                }
+//            }
         
         
         // Get all document data from collection users
@@ -88,7 +89,7 @@ class ViewController: UIViewController {
         
         
         // Get document data from particular user with ID
-//        let docRef = db.collection("users").document("cma5EgufnDsWMz9Hi1n5")
+//        let docRef = db.collection("users").document("\(userID)")
 //
 //        docRef.getDocument { (document, error) in
 //            if let document = document, document.exists {
@@ -98,6 +99,25 @@ class ViewController: UIViewController {
 //                print("Document does not exist")
 //            }
 //        }
+        
+        
+        let jo2Ref = db.collection("users").document("\(userID)")
+        
+        // Atomically add a new region to the "regions" array field.
+        jo2Ref.updateData([
+            "friends": FieldValue.arrayUnion([
+                    [
+                    "id": "token2",
+                    "statusCode": 1
+                    ]
+                ])
+            ])
+        
+        // Atomically remove a region from the "regions" array field.
+//        washingtonRef.updateData([
+//            "regions": FieldValue.arrayRemove(["east_coast"])
+//            ])
+
     }
 
 }
